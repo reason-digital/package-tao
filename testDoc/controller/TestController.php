a234abd84ba825076c111271afc904f2d690d6c6
ben sheridan <ben.sheridan@reasondigital.com>
2017-12-08 09:44:29 +0000

    working conteoller and extension

---
+++ testDoc/controller/TestController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace oat\testDocs\controller;
+
+class TestController extends \tao_actions_CommonModule
+{
+    public function sayHello()
+    {
+        echo 'Hello World';
+    }
+}
